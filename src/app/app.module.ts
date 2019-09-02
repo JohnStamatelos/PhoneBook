f446b946f78ebde54a563cf079ce55ba20de779d
John Stamatelos <JStamatelos@DVTL101082.local>
2019-09-02 10:25:18 +0200

    Scafolded header, home-page and add-item page. Moved toolbar into header component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomePageComponent } from './home-page/home-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AddItemComponent } from './components/add-item/add-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    HeaderComponent,
+    AddItemComponent
   ],
   imports: [
     BrowserModule,
