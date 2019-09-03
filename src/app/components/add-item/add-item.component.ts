b5d53cd7802774dcc22d8e50b0235ace852a24f5
JohnStamatelos <stamatelos.i@gmail.com>
2019-09-03 11:02:17 +0200

    Reactive form validation added.

--- src/app/components/add-item/add-item.component.ts
+++ src/app/components/add-item/add-item.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, RequiredValidator } from '@angular/forms';
-import {  } from '@angular/forms';
+import { FormGroup, FormBuilder, RequiredValidator, Validators } from '@angular/forms';
+import { } from '@angular/forms';
 
 import { IPhoneBookItem } from 'src/app/models/iphone-book-item';
 
@@ -10,20 +10,20 @@ import { IPhoneBookItem } from 'src/app/models/iphone-book-item';
   styleUrls: ['./add-item.component.scss']
 })
 export class AddItemComponent implements OnInit {
-  form : FormGroup;
-  item : IPhoneBookItem;
+  form: FormGroup;
+  item: IPhoneBookItem;
 
-  constructor(private fb : FormBuilder) { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.form = this.fb.group({
-      itemName: [''],
-      itemSurname: '',
-      itemPhone : ''
+      itemName: ['', [Validators.required, Validators.maxLength(20), Validators.minLength(2)]],
+      itemPhone: ['', [Validators.required, Validators.pattern(/^0((60[3-9]|64[0-5]|66[0-5])\d{6}|(7[1-4689]|6[1-3]|8[1-4])\d{7})$/)]],
+      itemSurname: ['', [Validators.required, Validators.maxLength(20), Validators.minLength(2)]],
     })
   }
 
-  saveItem(): void{
+  saveItem(): void {
     console.log(this.form)
   }
 }
