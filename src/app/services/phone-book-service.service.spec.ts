1abaa98d55616ab7ebcd2cf2ac843fa97b340b58
JohnStamatelos <stamatelos.i@gmail.com>
2019-09-04 09:07:01 +0200

    fixed unit test - added imports [ ]

--- src/app/services/phone-book-service.service.spec.ts
+++ src/app/services/phone-book-service.service.spec.ts
@@ -1,9 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PhoneBookService } from './phone-book-service.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('PhoneBookService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule]
+    })
+  });
 
   it('should be created', () => {
     const service: PhoneBookService = TestBed.get(PhoneBookService);
